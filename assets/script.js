e6a158ee89720f888051e24de698e768ea091fb1
lana <lanabkim@gmail.com>
2020-11-30 00:36:54 -0800

    clear files

--- assets/script.js
+++ assets/script.js
@@ -1,191 +1,218 @@
-//  my api - b735a5eb039f390c27f374f7010e73a3
+
 
 
 $(function(){
+    $('.form-group').on('submit' , function(e){
+        e.preventDefault(); 
+    })
+    
+        const myAPI = "b735a5eb039f390c27f374f7010e73a3"; 
 
-    // getting array for pastCitySearch in localStorage or empty array if there is none 
-    let pastCityList = JSON.parse(localStorage.getItem('pastCitySearch')) || []; 
-  
-    // if (pastCityList.length > 0 ) { 
-    //     // if there is any city in pastCityList array, 
-    //     pastCityList.forEach(function(past){
-    //         // Loop thru forEach item in array and 
-    //         console.log(past)
-    //         const pastDisplayBtn = $(`<button type="button" class="list-group-item list-group-item-action"> 
-    //             ${past} </button>`); 
+        let pastCityList = JSON.parse(localStorage.getItem('pastCitySearch')) || []; 
+        
+        if ( pastCityList.length > 0 ) {
+            for ( i = 0; i < pastCityList.length ; i++) {
+
+                const pastDisplayBtn = $(`<button type="button" data-name ="${pastCityList[i]}" class="button list-group-item list-group-item-action"> 
+                ${pastCityList[i]} </button>`); 
+                $('#past-search').prepend(pastDisplayBtn); 
+
+                
+            }
             
-    //         $('#past-search').append(pastDisplayBtn); 
-    //         // render to HTML
-    //     })
-    // }
-
-        // seatch button click 
-    // $('.btn').click(cityWeather); 
-    $('.btn').click(function(){
-        
-        let newCity = $('#city-search-input').val().trim(); 
-        
-        cityWeather(newCity); 
-
-        
-        if (pastCityList.indexOf(newCity) === -1  ||  newCity  !== "") {
-            pastCityList.push(newCity); 
+            console.log(pastCityList); 
         }
-       
-        if (pastCityList.length > 0 || !pastCityList.includes(newCity) ) { 
-            // if there is any city in pastCityList array, 
-            pastCityList.forEach(function(newCity){
-                // Loop thru forEach item in array and 
+    
+  
+     // create FUNCTION for displaying cityweather 
+        function cityWeather(city) { 
+            // empty before appending new one 
+            $('#city-weather-display').empty(); 
+            $('#5-day-forecast-display').empty(); 
+            $('#uvi').removeClass(); 
+            
+
+    
+            const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + myAPI; 
+            // used Imperial for measurement unit 
+            $.ajax({
+                url: queryURL, 
+                method: "GET"
+            })
+            .then(function(response){
+                console.log(response)
+                // console.log(response.descrip)
+               
+                console.log(response.weather[0].icon); 
+                console.log(response.coord.lat); 
+                console.log(response.coord.lon); 
                 
-                const pastDisplayBtn = $(`<button type="button" class="button list-group-item list-group-item-action"> 
-                    ${newCity} </button>`); 
+                const weatherIcon = response.weather[0].icon;
+
+                const iconURL=  "https://openweathermap.org/img/wn/" + weatherIcon + ".png"
+                console.log(iconURL)
+                const date = moment.unix(response.dt).format('MM/DD/YYYY'); 
+                console.log(date); 
                 
-                $('#past-search').append(pastDisplayBtn); 
-                // render to HTML
+                // dinamically creating HTML 
+                
+                const cityWeatherDisplay = `
+                <h5> <b> ${ response.name } </b> <span> (${ date }) </span> <img src = "${iconURL}"> </h5>
+                <p> Description: ${ response.weather[0].main } </p>
+                <p> Temperature: ${ response.main.temp }   °F </P>
+                <p> Humidity: ${ response.main.humidity } % </p>
+                <p> Wind Speed: ${ response.wind.speed } MPH </P>
+                `
+                
+                $('#city-weather-display').append(cityWeatherDisplay); 
+                // appeding to HTML
+                const lat = (response.coord.lat); 
+                const lon = (response.coord.lon); 
+                // const lat = parseInt(response.coord.lat); 
+    
+                getUV(lat, lon); 
+                // console.log(lat); 
+                getFiveDays(lat, lon); 
+    
             })
         }
-        addToLocalStorage(newCity); 
-
-    }); 
+    
+        function getUV(lat, lon){ 
+    
+            const uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + myAPI; 
+    
+            $.ajax({
+                url: uvURL, 
+                method: "GET"
+            })
+            .then(function(response){
+                console.log(response); 
 
     
+                console.log(response.current.uvi); 
+                
+                const uvIndex = response.current.uvi; 
+                const uvDispay = `<p> UV Index: <span id = "uvi"> ${ uvIndex } </span> </p>`
+                console.log(uvIndex , 'uvindex')
+                // $('#uvi').text(uvIndex); 
+               
 
-    // ***** FUNCTION adding input city to pasrCityList array 
-    function addToLocalStorage () { 
-        // pastCityList.push(city); 
-        let citySting = JSON.stringify(pastCityList); 
-        //Converts a JavaScript value (pastCityList) to a JSON string.
-        localStorage.setItem('pastCitySearch' , citySting); 
-        // and set it to localStorage array/pastCitySearch 
-    }
+                $('#uvi').removeClass(); 
+                
+                // 1-2 Low green , 3-5 yellow Moderate  , 6-7 High orage , 8-10 Red Very High , 11+ Purple Extreme 
+                // THIS IS NOT WORKING 
+                // SOMETHNG IS NOT RIGHT
+                if ( uvIndex < 3 ) {
+                    $('#uvi').addClass("text-white bg-success"); 
+                } 
+                else if( uvIndex >= 3 && uvIndex < 6  ){
+                    $('#uvi').addClass("text-white bg-warning"); 
+                }
+                else if( uvIndex >= 6 && uvIndex< 8  ){
+                    $('#uvi').addClass("text-white bg-orange"); 
 
+                }
+                else if( uvIndex >= 8 && uvIndex < 11  ){
+                    $('#uvi').addClass("text-white bg-danger"); 
+                }
+                else if( uvIndex > 11  ){
+                    $('#uvi').addClass("text-white bg-purple"); 
+                }
 
-
-
-    const myAPI = "b735a5eb039f390c27f374f7010e73a3"; 
-
-//     })
- // create FUNCTION for displaying cityweather -> later pass on click  $ for enter
-//  let city = $('#city-search-input').val().trim(); 
-    function cityWeather(newCity) { 
-        // need to empty before appending new one 
-
-        // let city = $('#city-search-input').val().trim(); 
-        // this will only get city value from userINPUT // need to change for Local Storage
-        const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&units=imperial&appid=" + myAPI; 
-        // used Imperial for measurement unit 
-        $.ajax({
-            url: queryURL, 
-            method: "GET"
-        })
-        .then(function(response){
-            console.log(response)
-            // console.log(response.descrip)
-           
-            console.log(response.weather[0].icon)
-            // UV index is with lat & lon only? 
-            console.log(response.coord.lat); 
-            console.log(response.coord.lon); 
-            let weatherIcon = response.weather[0].icon; 
-            iconURL=  "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
-            // iconURL = "http://openweathermap.org/img/wn/10d@2x.png"
-
-
+                $('#city-weather-display').last().append(uvDispay); 
+    
+            })
+    
+        }
+    
+        function getFiveDays(lat, lon){
             
-            // dinamically creating HTML ****************   NEED TO GET TIME & ICON
-            const cityWeatherDisplay = `
-            <h5> ${ response.name } <span> ${ response.dt} </span> <img src = iconURL> </h5>
-            <p> Description: ${ response.weather[0].main } </p>
-            <p> Temperature: ${ response.main.temp }  °F </P>
-            <p> Humidity: ${ response.main.humidity } % </p>
-            <p> Wind Speed: ${ response.wind.speed } MPH </P>
-            `
-            $('#city-weather-display').append(cityWeatherDisplay); 
-            // appeding to HTML
-            // const lat = (response.coord.lat); 
-            // const lon = (response.coord.lon); 
-            // const lat = parseInt(response.coord.lat); 
-            const lat = response.coord.lat; 
-            const lon = response.coord.lon; 
+            const uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + myAPI; 
+    
+            $.ajax({
+                url: uvURL, 
+                method: "GET"
+            })
+            .then(function(response){
 
-            getUV(lat, lon); 
-            console.log('qhat' , lat); 
+                for (i = 1; i < 6 ; i++ ){
 
+                    const forecastDate = moment.unix(response.daily[1].dt).format('MM/DD/YYYY');
+                    const forecaseIcon = response.daily[0].weather[0].icon; 
+                    const forecaseIconURL =  "https://openweathermap.org/img/wn/" + forecaseIcon + "@2x.png"
+                    const forecastDisplay = `
+                    <div class="col-sm-4">
+                        <div class="card" >
+                        <div class="card-body bg-primary text-white rounded">
+                        <h4 class="card-title"> ${forecastDate} </h4>
+                        <img class="card-text" src = "${ forecaseIconURL}">
+                        <p class="card-text">Temp: ${ response.daily[0].temp.day }  °F </p>
+                        <p class="card-text">Humidity: ${response.daily[0].humidity}   %</p>
+                    </div>
+                  </div>
+                </div>`
+                    $('#5-day-forecast-display').append(forecastDisplay)
 
-        })
-    }
+                }
+                // const forecasedate = moment.unix(response.daily[1].dt).format('MM/DD/YYYY');
+                // console.log(forecastdate) 
+                console.log(response.daily[0].dt)
 
-    function getUV(lat, lon){ 
+                console.log(response.daily[0].weather[0].icon); 
+                console.log(response.daily[0].temp.day); 
+                console.log(response.daily[0].humidity); 
 
-        const uvURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + myAPI; 
+            })
 
-        $.ajax({
-            url: uvURL, 
-            method: "GET"
-        })
-        .then(function(response){
-            console.log(response); 
-            console.log(response[0].value); 
+        }
 
-            //got UV index!!
-
-        })
-
-
-
-
+    
+    
         
+        // seatch button click 
+        // $(".btn").click(cityweather); 
+        $('#search-btn').on('click', function(e){
+            e.preventDefault(); 
+            
+            const newCity = $('#city-search-input').val().trim().toUpperCase(); 
+            
+            if (!pastCityList.includes(newCity) && newCity !== ""){
 
-    }
+                const newCityBtn = $(`<button type="button" data-name = "${newCity}" class="button list-group-item list-group-item-action"> 
+                    ${newCity} </button>`); 
+                $('#past-search').prepend(newCityBtn); 
+                pastCityList.push(newCity); 
+                localStorage.setItem('pastCitySearch', JSON.stringify(pastCityList)); 
+
+            }
+            cityWeather(newCity); 
+        })
+        // changed button type from button to submit abling enterkey  submit !!! 
 
 
+        $('#past-search').on('click' , 'button' , function(){
+            const cityClicked = $(this).data('name'); 
+            console.log(this); 
+            cityWeather(cityClicked); 
+            console.log(cityClicked , '168')
+        })
 
+        $('#clear-btn').on('click', function(){
+            localStorage.removeItem('pastCitySearch'); 
+            $('#past-search').empty(); 
+            $('#city-weather-display').empty(); 
+            $('#5-day-forecast-display').empty(); 
+           
+        })
 
     
-
-
-
+  
     
-    // creat array for city searched and stored in localST
-    // initial array would be empty 
-
-    // need to gather search input from user and get store it localST 
-    // 
-
-
-    // creating function that will display weather with user input 
-    // function displayCityWeather(){ 
-
-        // const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b735a5eb039f390c27f374f7010e73a3"; 
-        // const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b735a5eb039f390c27f374f7010e73a3"; 
-        // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-        // api.openweathermap.org/data/2.5/find?q=London&units=imperial -> For temperature in Fahrenheit use units=imperial
-
-
-
-        // need to create AJAX call to get weatherinfo 
-        // need to change html id with _ instead of - -> will use id here instead of creating vars
-
-        // 
-        // $.ajax({
-        //     url:queryURL, 
-        //     method: "GET"
-        // })
-        // .then(function(response){
-        //     console.log(response)
-        // })
-    // }
-
-
-    console.log('what'); 
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
 })
